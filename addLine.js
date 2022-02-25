01babd695c68f4851a4786b8b18f0c9238c1a111
MonguDykrai <kisaihou@gmail.com>
2022-02-25 11:14:31 +0800

    addLine方法添加注释

--- addLine.js
+++ addLine.js
@@ -2,8 +2,15 @@ function addLine(id, x1, y1, x2, y2) {
   const canvas = document.getElementById(id);
   const newLine = document.createElement('div');
   newLine.classList.add('line');
+  // 求两点之间的距离 https://zhidao.baidu.com/question/2122016758828229227.html
+  // Math.sqrt() 函数返回一个数的平方根
+  // Math.pow() 函数返回基数（base）的指数（exponent）次幂，即 base^exponent。
   const length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-  const ang = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
+  // Math.atan2() 返回从原点(0,0)到(x,y)点的线段与x轴正方向之间的平面角度(弧度值)，也就是Math.atan2(y,x)
+  // Math.atan2((y2 - y1), (x2 - x1)) 求弧度
+  // 弧度 * (180 / Math.PI) 求角度
+  const ang = Math.atan2(y2 - y1, x2 - x1) * (180 / Math.PI);
+  // const ang = (Math.atan2(y2 - y1, x2 - x1) * 180) / Math.PI;
   newLine.style.top = `${x1}px`;
   newLine.style.left = `${y1}px`;
   newLine.style.width = `${length}px`;
@@ -12,6 +19,7 @@ function addLine(id, x1, y1, x2, y2) {
   newLine.addEventListener('click', (e) => {
     console.log(e);
   });
+
   canvas.appendChild(newLine);
 }
 
